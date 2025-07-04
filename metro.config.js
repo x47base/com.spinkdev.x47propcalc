1eec0a173626637111e6add6cc6243e32a911834
Samuel Spink <dr.greenjet@gmail.com>
2025-07-04 09:36:01 +0200

    .

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,33 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig: getRNConfig } = require('@react-native/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const expoConfig = getDefaultConfig(__dirname);
+const rnConfig = getRNConfig(__dirname);
+
+const config = {
+    ...expoConfig,
+    ...rnConfig,
+    resolver: {
+        ...expoConfig.resolver,
+        ...rnConfig.resolver,
+    },
+    transformer: {
+        ...expoConfig.transformer,
+        ...rnConfig.transformer,
+    },
+    serializer: {
+        ...expoConfig.serializer,
+        ...rnConfig.serializer,
+    },
+    server: {
+        ...expoConfig.server,
+        ...rnConfig.server,
+    },
+    symbolicator: {
+        ...expoConfig.symbolicator,
+        ...rnConfig.symbolicator,
+    },
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
